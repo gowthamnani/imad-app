06d3affcca52c9253a21115e9adb28b9832bed59
gowthamnani <gowti2011@gmail.com>
2017-08-07 18:41:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+ // change the text of the main-text div
+ var element = document.getElementById('main-text');
+ element.innerHTML = "New Value(web is under construction)";
+ 
+ //move the image
+ var img = document.getElementById('madi');
+ img.onclick = function(){
+    img.style.marginleft = '100px'; 
+ };
