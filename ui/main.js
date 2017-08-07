b992b34f1315ddfe477284c1b91bfc8b9a60da96
gowthamnani <gowti2011@gmail.com>
2017-08-07 20:36:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,23 @@
-console.log('Loaded!');
+var main = function(){
+    $('.icon-menu').click(function(){
+        $('.menu').animate({
+            left: '0px'
+            },200);
+            
+            $('body').animate({
+                left: '285px'
+                },200);
+            });
+    $('.icon-close').click(function(){
+        $('.menu').animate({
+            left: '-285px'
+            },200);
+        
+            $('body').animate({
+                left: '0px'
+            },200);
+    });
+};
 
- // change the text of the main-text div
- var element = document.getElementById('main-text');
- element.innerHTML = 'New Value(web is under construction)';
- 
- //move the image
- var img = document.getElementById('madi');
- var marginLeft = 0;
- function moveRight (){
-     marginLeft = marginLeft +1;
-     img.style.marginLeft = marginLeft + 'px';
- }
- img.onclick = function(){
-    var interval = setInterval(moveRight,50);
- };
+
+$(document).ready(main);
