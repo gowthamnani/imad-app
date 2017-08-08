03aa1fd226d6aa5f8a1b0e2fc43de519d287126d
gowthamnani <gowti2011@gmail.com>
2017-08-08 21:14:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,14 @@
-var main = function(){
-    $('.icon-menu').click(function(){
-        $('.menu').animate({
-            left: '0px'
-            },200);
-            
-            $('body').animate({
-                left: '285px'
-                },200);
-            });
-    $('.icon-close').click(function(){
-        $('.menu').animate({
-            left: '-285px'
-            },200);
-        
-            $('body').animate({
-                left: '0px'
-            },200);
-    });
+
+var button = dociment.getElementById('counter');
+var counter = 0;
+button.onclick = function () {
+    
+    // make a request to the counter endpoint
+    
+    //capture the response and store it in a variable
+    
+    // render the variable in the correct span
+    couter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.tostring();
 };
-
-
-$(document).ready(main);
